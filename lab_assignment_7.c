0ae191191e5f2a1359b05f382e827b5e4d43de7d
amosperez <38868190+amosperez@users.noreply.github.com>
2024-03-08 21:59:13 -0500

    Update lab_assignment_7.c

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -11,17 +11,20 @@ int main(){
         int array1[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
         int array2[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
 
-        // bubbleSort(array1, 9);
-        // bubbleSort(array2, 9);
+        bubbleSort(array1, 9);
+        bubbleSort(array2, 9);
 
-        selectionSort(array1, 9);
-        selectionSort(array2, 9);
+        int array3[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+        int array4[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
+
+        selectionSort(array3, 9);
+        selectionSort(array4, 9);
     
 }
 
 void bubbleSort(int array[], int size){
-
     int arrayFreq[100] = {0};
+    int totalSwaps = 0;
 
     int lastIndex;
     int left;
@@ -37,7 +40,7 @@ void bubbleSort(int array[], int size){
             if(array[left]>array[right]){
                 arrayFreq[array[left]]++;
                 arrayFreq[array[right]]++;
-
+                totalSwaps++;
                 temp = array[left];
                 array[left] = array[right];
                 array[right] = temp;
@@ -51,11 +54,13 @@ void bubbleSort(int array[], int size){
             printf("%d: # of times %d is swapped.\n", arrayFreq[i], i);
         }
     }
+    printf("total # of swaps: %d\n", totalSwaps);
     printf("\n\n");
 }
 
 void selectionSort(int array[], int size){
     int arrayFreq[100] = {0};
+    int totalSwaps = 0;
 
     int minimum;
     int temp;
@@ -70,6 +75,7 @@ void selectionSort(int array[], int size){
         if(array[i]!=array[minimum]){
             arrayFreq[array[i]]++;
             arrayFreq[array[minimum]]++;
+            totalSwaps++;
         }
 
 
@@ -83,6 +89,7 @@ void selectionSort(int array[], int size){
             printf("%d: # of times %d is swapped.\n", arrayFreq[i], i);
         }
     }
+    printf("total # of swaps: %d\n", totalSwaps);
     printf("\n\n");
 }
 
